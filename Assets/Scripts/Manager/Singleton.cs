716980d6b86f49d88e22969b62ec97ac9fc49a48
Zzondy-Unity <abdos230@naver.com>
2024-11-11 09:38:28 +0900

    Assets : Animation

---
+++ Assets/Scripts/Manager/Singleton.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+                if (instance == null)
+                {
+                    GameObject go = new GameObject(typeof(T).Name);
+                    go.AddComponent<T>();
+                    instance = go.GetComponent<T>();
+                    DontDestroyOnLoad(go);
+                }
+            }
+            return instance;
+        }
+    }
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
